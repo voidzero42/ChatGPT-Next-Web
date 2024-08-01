f679388095efc70a4a781505978e23ab9da0f8ac
voidzero <7929423+voidzero42@users.noreply.github.com>
2024-08-01 16:03:43 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -5,7 +5,7 @@ import styles from "./home.module.scss";
 import { IconButton } from "./button";
 import SettingsIcon from "../icons/settings.svg";
 import GithubIcon from "../icons/chatgpt.svg";
-import ChatGptIcon from "../icons/jw2.png";
+import ChatGptIcon from "../icons/jw2.svg";
 import AddIcon from "../icons/add.svg";
 import CloseIcon from "../icons/close.svg";
 import DeleteIcon from "../icons/delete.svg";
